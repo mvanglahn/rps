9180b928c330aed25ad4e8843d22193580367748
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-13 15:41:22 -0400

    Added 5 round game function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 var options = ['rock', 'paper', 'scissors'];
-
+let playerScore = 0;
+let cpuScore = 0;
 
 function getComputerChoice() {
     choiceNum = Math.floor(Math.random()*10/4);
@@ -19,18 +20,38 @@ function playRound(playerSelect, computerSelection){
         playerSelect === 'scissors' && computerSelection === 'paper' ||
         playerSelect === 'paper' && computerSelection === 'rock'){
             console.log("Player wins!", playerSelect, "beats", computerSelection)
+            playerScore++;
     } else if (
             playerSelect === 'scissors' && computerSelection === 'rock' ||
             playerSelect === 'paper' && computerSelection === 'scissors' ||
             playerSelect === 'rock' && computerSelection === 'paper') {
                 console.log("Computer wins!", playerSelect, "loses to", computerSelection)
+                cpuScore++;
     } else if (playerSelect === computerSelection){
         console.log("It's a tie! Try again")
     }
 }
 
+function game() {
+    if (i<6) {
+        var playerSelection = playerPrompt();
+        var computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log("Computer selected", computerSelection);
+        console.log("Score:");
+        console.log("Player:", playerScore, "CPU:", cpuScore);
+        i++;
+    } else if (i = 6) {
+        if (playerScore > cpuScore){
+            console.log("You win!");
+        } else if (playerScore < cpuScore){
+            console.log("CPU wins!");
+        } else {
+            console.log("It's a tie")
+        }
+    }
+}
+
 //const playerSelect = 'rock';
-var playerSelection = playerPrompt();
-var computerSelection = getComputerChoice();
-console.log("Computer selected", computerSelection);
-playRound(playerSelection, computerSelection);
+
+
