b1ad6f3dbd056a3d940944d0395ef4fc388b8b71
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-14 08:28:50 -0400

    Layout for webpage interface

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 var options = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let cpuScore = 0;
+let i =0;
 
 function getComputerChoice() {
     choiceNum = Math.floor(Math.random()*10/4);
@@ -33,25 +34,24 @@ function playRound(playerSelect, computerSelection){
 }
 
 function game() {
-    if (i<6) {
+    for (let i = 0; i < 5; i++) {
         var playerSelection = playerPrompt();
         var computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
         console.log("Computer selected", computerSelection);
         console.log("Score:");
         console.log("Player:", playerScore, "CPU:", cpuScore);
-        i++;
-    } else if (i = 6) {
-        if (playerScore > cpuScore){
-            console.log("You win!");
-        } else if (playerScore < cpuScore){
-            console.log("CPU wins!");
-        } else {
-            console.log("It's a tie")
-        }
-    }
+    } 
 }
 
-//const playerSelect = 'rock';
-
+game();
+if (i = 6) {
+    if (playerScore > cpuScore){
+        console.log("You win!");
+    } else if (playerScore < cpuScore){
+        console.log("CPU wins!");
+    } else {
+        console.log("It's a tie")
+    }
+}
 
